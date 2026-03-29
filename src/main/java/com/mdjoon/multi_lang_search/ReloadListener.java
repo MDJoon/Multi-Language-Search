d1d49622a4d07a5d86c1635974a571144d0e70ab
MDJoon <a01047768319@gmail.com>
2026-03-29 13:38:58 +0900

    reload searchtrees

--- src/main/java/com/mdjoon/multi_lang_search/ReloadListener.java
+++ src/main/java/com/mdjoon/multi_lang_search/ReloadListener.java
@@ -3,6 +3,8 @@ package com.mdjoon.multi_lang_search;
 import com.mdjoon.multi_lang_search.config.ConfigManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import org.jspecify.annotations.NonNull;
@@ -13,5 +15,10 @@ public class ReloadListener implements ResourceManagerReloadListener {
     public void onResourceManagerReload(@NonNull ResourceManager manager) {
         ConfigManager.load();
         MultiLanguageCache.reload();
+
+        LocalPlayer player = Minecraft.getInstance().player;
+        if(player != null) {
+            player.connection.updateSearchTrees();
+        }
     }
 }
